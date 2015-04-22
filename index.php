f0610d8b16a1a11b76d942bf933af115c1eacf7d
AntonElkin <antonelkin777@gmail.com>
2015-04-22 20:35:32 +0300

    1st
    Signed-off-by: AntonElkin <antonelkin777@gmail.com>

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+/*
+    echo "Hello,world!";
+	    $hello = "Hello";
+		$world = "world";
+		$greeting = $hello." ".$world;
+		echo $greeting;
+	
+	 $number = 42;
+	 $sum = $number + 10 -5 * 3;
+		echo "Result = ".$sum; //Result = 57
+	 $numbers = array ();
+	 $numbers = array (42,96);
+	 $numbers[]= 101;
+	 $numbers[] = "200"; //добаввляем строку
+	 echo $numbers [2]; // 101
+	 $numbers[1] = $numbers[0]+ $numbers[2];
+	 $numbers [0] = $numbers[0] + 100; //142
+*/
+/*
+			$number = 3
+			if ($number>5)
+			{	
+			$number *= 2;
+			}
+			else {
+			$number -=1;	
+			}
+	*/
+	/*
+		$number = array (0,1,2,3,6,24);
+		 for ($i = 0; $i < count ($numbers); $i++);
+		 {
+			 echo $numbers[$i];
+		 }
+		
+		$number = array (0,1,2,3,6,24);
+		 foreach ($numbers as $numbers);
+		  {
+			 echo $numbers[$i];
+		 }
+*/		
+		$numbers = array (0,1,2,3,6,24);
+			print_r ($numbers);
+			var_dump ($numbers);
+			
+	//ассоциативный массив
+	
+	$animals = array
+	(
+	"blue" => "elephant",
+	"brown" => "bear",
+	"black" => "cat"
+	);
