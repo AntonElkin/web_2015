723d4a733fbc8953e3126e858267d8ff866ff6f2
AntonElkin <antonelkin777@gmail.com>
2015-04-24 21:33:41 +0300

    Update print_name.php

--- print_name.php
+++ print_name.php
@@ -1,3 +1,9 @@
 <?php
     header('Content-Type: text/plain');
+    
+    $nameValue = '';
+    if (isset($_GET['name']))    
+    {
+        $nameValue= $_GET['name'];
+    }
     echo "Hello, Dear ".$_GET['name']."!";
