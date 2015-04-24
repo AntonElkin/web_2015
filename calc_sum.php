28dd9f5a37b31f6a051003ca75c1ab91a298136d
AntonElkin <antonelkin777@gmail.com>
2015-04-24 21:38:22 +0300

    Update calc_sum.php

--- calc_sum.php
+++ calc_sum.php
@@ -1,3 +1,13 @@
 <?php
-    $sum = $_GET["arg1"] + $_GET["arg2"];
+    $arg1 = 0;
+    if (isset($_GET["arg1"]))
+    {
+         $arg1= $_GET["arg1"];
+    }
+    $arg2 = 0;
+    if (isset($_GET["arg2"]))
+    {
+        $arg1= $_GET["arg2"];
+    }
+    $sum = $arg1 + $arg2;
     echo $sum;
