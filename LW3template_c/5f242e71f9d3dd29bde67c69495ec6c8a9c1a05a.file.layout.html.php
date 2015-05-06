0f08d88624aaae21b41664f2f2a0622b71200784
AntonElkin <antonelkin777@gmail.com>
2015-05-06 12:47:47 +0300

    LW3

---
+++ LW3template_c/5f242e71f9d3dd29bde67c69495ec6c8a9c1a05a.file.layout.html.php
@@ -0,0 +1,38 @@
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-05-06 11:44:33
+         compiled from "D:\web\server2go\htdocs\LW3\template\layout.html" */ ?>
+<?php /*%%SmartyHeaderCode:154685549e28147d380-40572539%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '5f242e71f9d3dd29bde67c69495ec6c8a9c1a05a' => 
+    array (
+      0 => 'D:\\web\\server2go\\htdocs\\LW3\\template\\layout.html',
+      1 => 1430905200,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '154685549e28147d380-40572539',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'content' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_5549e281488610_49214128',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5549e281488610_49214128')) {function content_5549e281488610_49214128($_smarty_tpl) {?><!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Template engine</title>    
+  </head>
+  <body>
+    <!-- place code for header here -->
+    <?php echo $_smarty_tpl->tpl_vars['content']->value;?>
+
+    <!-- place code for footer here -->
+  </body>
+</html><?php }} ?>
